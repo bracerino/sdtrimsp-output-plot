83e8fd776415aefa377801ad6994ac9aec1e5d0d
Miroslav Lebeda <149959159+bracerino@users.noreply.github.com>
2026-01-15 21:31:05 +0100

    Refactor software version notice for clarity

--- app.py
+++ app.py
@@ -939,9 +939,8 @@ def main():
     st.markdown(
         """
     Upload your SDTrimSP output files to visualize concentration profiles or select suitable mode from the sidebar.
-        **Software version notice:**  
-    The application is currently validated against **SDTrimSP 7.01 (7.00)**.  
-    Compatibility with newer versions will be implemented when possible and following updates to the publicly available documentation.
+        **Software version notice:** The application is currently valid for **SDTrimSP 7.01 (7.00)**. 
+        Compatibility with newer versions will be implemented when possible and following updates to the publicly available documentation.
     """
     )
     css = '''
